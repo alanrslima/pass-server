d0623fe998fbb17ceba8dbef615643bcac4ce4c1
alanrslima <alanronison.lima@gmail.com>
2020-03-17 21:41:22 -0300

    Ajustes

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 const express = require("express");
-// const cors = require("cors");
 
 const app = express();
+const pass = require("./passbook");
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-const passbook = require("passbook");
-// 9ZFP8JD2KJ
-let template = passbook("storeCard", {
-  passTypeIdentifier: "pass.br.com.sebrae.napalma",
-  teamIdentifier: "9ZFP8JD2KJ",
-  organizationName: "Sebrae",
-  description: "Description"
-});
-template.loadImagesFrom("./src/images");
-template.fields.barcode = {
-  message: "123456789",
-  format: "PKBarcodeFormatPDF417",
-  messageEncoding: "iso-8859-1"
-};
-template.fields.serialNumber = "12312";
-template.keys("./src/keys", "1234");
-
-let pass = template.createPass();
-
-app.get("/mypass", function(request, response) {
-  console.log(pass);
-  pass.render(response, function(error) {
+app.get("/pass", function (request, response) {
+  pass.render(response, function (error) {
     if (error) console.error(error);
   });
 });
