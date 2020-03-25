afb9b02f2a37e676ffcbcfcf5f64193a4cf7c741
alanrslima <alanronison.lima@gmail.com>
2020-03-25 20:06:12 -0300

    Retirado parametros (test case)

--- src/controllers/PassController.js
+++ src/controllers/PassController.js
@@ -3,63 +3,117 @@ const passbook = require("passbook");
 module.exports = {
   async index(request, response) {
 
-    const {
-      date,
-      hour,
-      place,
-      eventType,
-      additionalPlace,
-      eventName,
-      participant,
-      serialNumber,
-      qrdCodeMessage
-    } = request.body;
+    // const {
+    //   date,
+    //   hour,
+    //   place,
+    //   eventType,
+    //   additionalPlace,
+    //   eventName,
+    //   participant,
+    //   serialNumber,
+    //   qrdCodeMessage
+    // } = request.body;
+
+    // let template = passbook("eventTicket", {
+    //   formatVersion: 1,
+    //   passTypeIdentifier: "pass.br.com.sebrae.napalma",
+    //   teamIdentifier: "9ZFP8JD2KJ",
+    //   organizationName: "Sebrae",
+    //   description: eventName,
+    //   barcode: {
+    //     message: qrdCodeMessage,
+    //     format: "PKBarcodeFormatQR",
+    //     messageEncoding: "iso-8859-1"
+    //   },
+    //   serialNumber: serialNumber,
+    //   foregroundColor: 'rgb(255, 255, 255)',
+    //   backgroundColor: "rgb(7, 94, 167)",
+    //   eventTicket: {
+    //     headerFields: [
+    //       {
+    //         key: "date",
+    //         label: hour,
+    //         value: date,
+    //       }
+    //     ],
+    //     primaryFields: [
+    //       {
+    //         key: "event",
+    //         label: eventType,
+    //         value: eventName,
+    //       },
+    //     ],
+    //     secondaryFields: [
+    //       {
+    //         key: "participant",
+    //         label: "PARTICIPANTE",
+    //         value: participant
+    //       },
+    //     ],
+    //     auxiliaryFields: [
+    //       {
+    //         key: "location",
+    //         label: "LOCAL",
+    //         value: place,
+    //       },
+    //       {
+    //         key: 'location_aditional',
+    //         value: additionalPlace,
+    //         textAlignment: "PKTextAlignmentLeft"
+    //       }
+    //     ]
+    //   }
+    // });
+    // template.loadImagesFrom("./src/images");
+    // template.keys("./src/keys", "1234");
+    // const pass = template.createPass();
 
     let template = passbook("eventTicket", {
       formatVersion: 1,
       passTypeIdentifier: "pass.br.com.sebrae.napalma",
       teamIdentifier: "9ZFP8JD2KJ",
       organizationName: "Sebrae",
-      description: eventName,
+      description: 'Teste',
       barcode: {
-        message: qrdCodeMessage,
+        message: '1234',
         format: "PKBarcodeFormatQR",
         messageEncoding: "iso-8859-1"
       },
-      serialNumber: serialNumber,
+      serialNumber: "1234",
       foregroundColor: 'rgb(255, 255, 255)',
       backgroundColor: "rgb(7, 94, 167)",
       eventTicket: {
         headerFields: [
           {
             key: "date",
-            label: hour,
-            value: date,
+            label: "12:45",
+            value: "20/06/2020",
           }
         ],
         primaryFields: [
           {
             key: "event",
-            label: eventType,
-            value: eventName,
+            label: "PALESTRA",
+            value: "Nome da palestra",
           },
         ],
         secondaryFields: [
           {
             key: "participant",
             label: "PARTICIPANTE",
-            value: participant
+            value: "joão"
           },
         ],
         auxiliaryFields: [
           {
             key: "location",
             label: "LOCAL",
-            value: place,
+            value: "Sebrae",
           },
           {
             key: 'location_aditional',
-            value: additionalPlace,
+            value: "Guara",
             textAlignment: "PKTextAlignmentLeft"
           }
         ]
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router();
 const passController = require("./controllers/PassController");
 
-routes.post("/pass", passController.index);
+routes.get("/pass", passController.index);
 
 module.exports = routes;
